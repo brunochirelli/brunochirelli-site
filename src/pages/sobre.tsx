30afbd9ac6dfdf17dcd0282e030f45da79237327
Bruno <brunochirelli@gmail.com>
2021-05-07 14:03:01 -0300

    add new about page

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { graphql } from "gatsby";
 
+import Layout from "../components/Layout";
 import PageHeader from "../components/PageHeader";
+import Article from "../components/Article";
 
 /**
  * About Page
@@ -9,17 +12,37 @@ import PageHeader from "../components/PageHeader";
  * @keywords
  */
 
-const AboutPage = () => {
+type AboutPageProps = {
+  data: any;
+};
+
+const AboutPage = ({ data }: AboutPageProps) => {
+  const { html } = data.markdownRemark;
+
   return (
-    <>
+    <Layout seo={{ title: "Sobre - Bruno Chirelli" }}>
       <PageHeader
         title="Quem é Bruno Chirelli"
         overline="Curiosidade, estudo e mãos à obra"
         lead="Saiba um pouco mais sobre mim e entenda um pouco mais a forma como penso"
         center
       />
-    </>
+
+      <Article html={html} />
+    </Layout>
   );
 };
+
+export const query = graphql`
+  query {
+    markdownRemark(frontmatter: { slug: { eq: "sobre" } }) {
+      html
+      frontmatter {
+        slug
+        title
+      }
+    }
+  }
+`;
 
 export default AboutPage;
