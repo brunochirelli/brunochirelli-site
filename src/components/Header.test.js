a4a26bcbfaff9b9295590bb607588ad903ebf14b
Bruno <brunochirelli@gmail.com>
2021-05-07 13:54:46 -0300

    add new print width

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -5,15 +5,9 @@ import { render } from "../../test-utils";
 import Header from "./Header";
 
 test("Displays the correct title", () => {
-  const { getByTestId, getByText } = render(
-    <Header siteTitle="One-Day-Site" />
-  );
+  const { getByTestId, getByText } = render(<Header siteTitle="One-Day-Site" />);
   // Assertion
-  expect(getByTestId("header-title")).toHaveTextContent(
-    "One-Day-Site"
-  );
+  expect(getByTestId("header-title")).toHaveTextContent("One-Day-Site");
 
-  expect(getByText("switch to light")).toHaveTextContent(
-    "switch to light"
-  );
+  expect(getByText("switch to light")).toHaveTextContent("switch to light");
 });
