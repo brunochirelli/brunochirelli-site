44f4d9a8ed0da13719d4d5a88ba942f81872eee8
Bruno <brunochirelli@gmail.com>
2021-05-07 13:58:53 -0300

    add new layout with theming feature

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,12 @@
 import React, { ReactElement, ReactNode } from "react";
 import styled from "styled-components";
 
-import Header from "./Header";
 import SEO, { SEOProps } from "./SEO";
-import UserConsent from "./UserConsent";
+// import UserConsent from "./UserConsent";
+import Header from "./Header";
+import Footer from "./Footer";
+import { useSelector } from "react-redux";
+import { RootState } from "../stores/store";
 
 type LayoutProps = {
   children: ReactNode;
@@ -11,18 +14,25 @@ type LayoutProps = {
 };
 
 const Layout = ({ children, seo }: LayoutProps): ReactElement => {
+  const fontBase = useSelector((state: RootState) => state.theme.fontBase);
+
   return (
-    <LayoutStyled>
-      <UserConsent />
+    <LayoutStyled fontBase={fontBase}>
       <SEO {...seo} />
       <Header siteTitle="One-Day-Site" />
-      <main>{children}</main>
+
+      <main id="content">{children}</main>
+
+      <Footer />
     </LayoutStyled>
   );
 };
 
 const LayoutStyled = styled.div`
   z-index: ${({ theme }) => theme.zIndex.default};
+  color: ${({ theme }) => theme.textColor};
+  font-size: ${({ fontBase }: { fontBase: number }) => fontBase}px;
+  background: ${({ theme }) => theme.background.default};
 `;
 
 export default Layout;
