605a27f755fe857e36f26452d74c8a1fa8a68000
Bruno <brunochirelli@gmail.com>
2021-05-12 09:17:38 -0300

    move content to another component

--- src/pages/contato.tsx
+++ src/pages/contato.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import { navigate } from "gatsby";
+import Layout from "../components/Layout";
+import { Checkbox, Container, Input, TextField } from "@material-ui/core";
+import { Controller, useForm } from "react-hook-form";
+import FormContact from "../components/FormContact";
 
 function encode(data: any) {
   return Object.keys(data)
@@ -37,70 +41,58 @@ const Contato = () => {
    *
    * https://github.com/sw-yx/gatsby-netlify-form-example-v2/blob/master/src/pages/contact.js
    */
-  const handleSubmit = (e: React.FormEvent): void => {
-    e.preventDefault();
+  // const handleSubmit = (e: React.FormEvent): void => {
+  //   e.preventDefault();
 
-    /** @TODO re-type 'form' */
-    const form: any = e.target;
+  //   /** @TODO re-type 'form' */
+  //   const form: any = e.target;
 
-    fetch("/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...formValues,
-      }),
-    })
-      .then(() => navigate(form.getAttribute("action")))
-      .catch(error => alert(error));
-  };
+  //   fetch("/", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/x-www-form-urlencoded",
+  //     },
+  //     body: encode({
+  //       "form-name": form.getAttribute("name"),
+  //       ...formValues,
+  //     }),
+  //   })
+  //     .then(() => navigate(form.getAttribute("action")))
+  //     .catch(error => alert(error));
+  // };
+
   return (
-    <div>
-      <form
+    <Layout seo={{ title: "Contato | Bruno Chirelli" }}>
+      {/* <form
         name="contato"
         method="post"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
         action="/sucesso"
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         id="contato-page-form"
-      >
-        {/* Netlify Form - just for the bots */}
-        <input
+      > */}
+      {/* Netlify Form - just for the bots */}
+      {/* <input
           type="hidden"
           name="form-name"
           value="contato"
           onChange={handleChange}
-        ></input>
-        <p hidden>
+        ></input> */}
+      {/* <p hidden>
           <label>
             Don’t fill this out: <input name="bot-field" onChange={handleChange} />
           </label>
-        </p>
-        {/* end Netlify Form */}
+        </p> */}
+      {/* end Netlify Form */}
 
-        <label>
-          Your Name: <input type="text" name="name" onChange={handleChange} />
-        </label>
-        <label>
-          Your Email: <input type="email" name="email" onChange={handleChange} />
-        </label>
-        <label>
-          Your Role:{" "}
-          <select name="role" multiple onSelect={handleChange}>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
-        </label>
-        <label>
-          Message: <textarea name="message" onChange={handleChange}></textarea>
-        </label>
-
-        <button type="submit">Enviar</button>
-      </form>
-    </div>
+      {/* my form */}
+      {/* <Container> */}
+      <FormContact />
+      {/* <button type="submit">Enviar</button> */}
+      {/* </Container> */}
+      {/* </form> */}
+    </Layout>
   );
 };
 
