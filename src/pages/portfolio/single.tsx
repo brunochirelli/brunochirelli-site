f7b6f87073abbb184cdeee468133697c7113867c
Bruno <brunochirelli@gmail.com>
2021-05-13 09:47:20 -0300

    add portfolio single placeholder page

---
+++ src/pages/portfolio/single.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Article from "../../components/Article";
+import ArticleHeader from "../../components/ArticleHeader";
+import Layout from "../../components/Layout";
+
+const PortfolioSingle = () => {
+  return (
+    <Layout>
+      <ArticleHeader />
+
+      <Article />
+    </Layout>
+  );
+};
+
+export default PortfolioSingle;
