7dea3e533f932b4cf0eb10db7892e2ef8e9183b1
Bruno <brunochirelli@gmail.com>
2021-05-13 09:46:57 -0300

    add new article styles

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import parse from "html-react-parser";
 
-import { Container } from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import { RootState } from "../stores/store";
 
 type ArticleProps = {
-  html: any;
+  html?: any;
 };
 
 const Article = ({ html }: ArticleProps) => {
@@ -15,22 +14,58 @@ const Article = ({ html }: ArticleProps) => {
 
   return (
     <StyledContainer component="article" maxWidth="md" fontBase={fontBase}>
-      {parse(html)}
+      <Grid container>
+        <Grid item md={1}></Grid>
+        <Grid className="article-text" item xs={12} sm={10}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae turpis
+            a nisl rhoncus tincidunt. Vivamus a pulvinar lacus. Donec purus sapien,
+            finibus sit amet odio nec, blandit accumsan tellus. Nunc vestibulum vestibulum
+            auctor.
+          </p>
+          <p>
+            In at enim urna. Pellentesque eget nunc sed urna commodo cursus. Aliquam
+            tempus porttitor odio ac blandit. Fusce euismod maximus neque, at elementum
+            ante efficitur eu.
+          </p>
+          <img
+            src="https://images.unsplash.com/photo-1491637639811-60e2756cc1c7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=669&q=80"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1517142089942-ba376ce32a2e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+            alt=""
+          />
+        </Grid>
+        <Grid item md={1}></Grid>
+      </Grid>
     </StyledContainer>
   );
 };
 
 const StyledContainer: any = styled(Container)`
-  font-size: ${({ fontBase }: { fontBase: number }) => fontBase}px;
-  line-height: 1.75em;
+  .article-text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
-  p {
-    margin-bottom: 1.75em;
-  }
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p,
+    ul,
+    blockquote {
+      max-width: 33rem;
+      line-height: 1.8;
+    }
 
-  h2 {
-    margin-top: 1.75em;
-    /* margin-bottom: 1em; */
+    img {
+      width: 100%;
+      margin-top: 1rem;
+    }
   }
 `;
 
