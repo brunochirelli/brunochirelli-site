98bd667ebd45366ea8d5eeb112bd6d348af8974e
Bruno <brunochirelli@gmail.com>
2021-05-14 14:40:34 -0300

    refactor component to hold optinal sketches

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -6,25 +6,59 @@ import { Container } from "@material-ui/core";
 import Button from "./Button";
 
 type PageHeaderProps = {
+  /**
+   * Title should be more than 4 words to occupy the white space.
+   */
   title: string;
+  /**
+   * Text to be shown in the button.
+   */
+  ctaText?: string;
+  /**
+   * Could be any internal or anchored link
+   */
+  ctaLink?: string;
+  /**
+   * Any svg or image. Need understand how to type it.
+   */
   sketch?: any;
+  hideCta?: boolean;
 };
 
-const PageHeader = ({ title, sketch }: PageHeaderProps) => {
+const PageHeader = ({
+  title,
+  sketch,
+  ctaText = "Entre em contato",
+  ctaLink = "/contato",
+  hideCta,
+}: PageHeaderProps) => {
+  const anchored = ctaLink?.split("")[0] === "#" ? true : false;
+
   return (
-    <StyledHeader>
+    <StyledHeader sketch={sketch}>
       <Container>
         <StyledContent>
           {sketch && <img className="sketch" src={sketch} alt="" />}
           <h1>{title}</h1>
-          <Button to="/contato">Entre em contato</Button>
+          {!hideCta && (
+            <Button to={ctaLink} variant={anchored ? "secondary" : "primary"}>
+              {ctaText}
+            </Button>
+          )}
         </StyledContent>
       </Container>
     </StyledHeader>
   );
 };
 
-const StyledHeader = styled.header`
+type StyledHeaderProps = {
+  /**
+   * Helps decide which padding the component will use if a sketch is present or not
+   */
+  sketch?: boolean;
+};
+
+const StyledHeader = styled.header<StyledHeaderProps>`
   display: flex;
   align-items: center;
   padding-top: 6rem;
@@ -33,7 +67,7 @@ const StyledHeader = styled.header`
   background: #f4f2ec;
 
   @media screen and (min-width: 600px) {
-    padding-top: 8rem;
+    padding-top: ${({ sketch }) => (sketch ? `8rem` : `4rem`)};
   }
 `;
 
