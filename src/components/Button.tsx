983bacc0f0f4f074f31a2c5c869243c56df6ed1f
Bruno <brunochirelli@gmail.com>
2021-05-15 07:30:05 -0300

    add anchored logic

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,21 +2,48 @@ import React, { ReactNode } from "react";
 import styled from "styled-components";
 import { Link } from "gatsby";
 
-import { FiArrowRight } from "react-icons/fi";
+import { FiArrowRight, FiArrowDown } from "react-icons/fi";
 
 type ButtonProps = {
   children: ReactNode;
   to: string;
-  variant: "primary" | "secondary";
+  onClick?: any;
 };
 
-const Button = ({ to, children, variant }: ButtonProps) => {
+const Button = ({ to, children }: ButtonProps) => {
+  // Check if the link is to an anchor to decide wich style of button will be outputed
+  const anchored = to?.split("")[0] === "#" ? true : false;
+
+  const handleScroll = () => {
+    // Grab the "to" variable and select the element and scroll to them
+    document.querySelector(to)?.scrollIntoView();
+  };
+
+  if (anchored)
+    return (
+      <StyledButton
+        variant="secondary"
+        role="button"
+        aria-label="Descer até o conteúdo"
+        onClick={handleScroll}
+      >
+        <div className="anchored">
+          <div className="icon">
+            <FiArrowDown />
+          </div>
+          <div>{children}</div>
+        </div>
+      </StyledButton>
+    );
+
   return (
-    <StyledButton to={to} variant={variant}>
-      <div className="icon">
-        <FiArrowRight />
-      </div>
-      <div>{children}</div>
+    <StyledButton variant="primary" role="button" aria-label="Ir para outra página">
+      <Link to={to}>
+        <div className="icon">
+          <FiArrowRight />
+        </div>
+        <div>{children}</div>
+      </Link>
     </StyledButton>
   );
 };
@@ -25,13 +52,18 @@ type StyledButtonProps = {
   variant: "primary" | "secondary";
 };
 
-const StyledButton = styled(Link)<StyledButtonProps>`
-  display: flex;
-  align-items: center;
-  color: ${({ theme, variant }) =>
-    variant === "primary" ? theme.palette.primary.main : theme.palette.secondary.main};
-  font-weight: bolder;
-  text-decoration: none;
+const StyledButton = styled.div<StyledButtonProps>`
+  cursor: pointer;
+
+  a,
+  .anchored {
+    display: flex;
+    align-items: center;
+    color: ${({ theme, variant }) =>
+      variant === "primary" ? theme.palette.primary.main : theme.palette.secondary.main};
+    font-weight: bolder;
+    text-decoration: none;
+  }
 
   .icon {
     display: flex;
@@ -44,8 +76,6 @@ const StyledButton = styled(Link)<StyledButtonProps>`
     background: ${({ theme, variant }) =>
       variant === "primary" ? theme.palette.primary.main : theme.palette.secondary.main};
     border-radius: 50%;
-    transform: ${({ variant }) =>
-      variant === "primary" ? "rotate(0)" : "rotate(90deg)"};
   }
 `;
 
