432b809668de917db166f99678b25ddcb7777c4b
Bruno Chirelli <brunochirelli@gmail.com>
2024-10-28 16:15:13 -0300

    setup tinacms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import ClientComponentExample from "@/components/ClientComponentExample";
-
 export default function Home() {
   return (
     <main>
       <h1 className="text-2xl font-bold">Home</h1>
-      <ClientComponentExample />
     </main>
   );
 }
